a263cf9b77d6e0a118fc3791d61e27284cb9a81e
pradumagnihotri <pradumagnihotri08@gmail.com>
2025-08-28 23:06:39 +0530

    updated file name

--- StreamAPI/src/Problem3.java
+++ StreamAPI/src/Problem3.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 // filtering integer
 
-public class Project3 {
+public class Problem3 {
     public static void main(String[] args) {
 //        filter the element using filter()
         List<Integer> numbers = Arrays.asList(10,15,9,45,55,13);
